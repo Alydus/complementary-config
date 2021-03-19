8e51ee91d01095044b4f808ae3c671a7586ab4da
Alydus <officialalydus@gmail.com>
2021-03-19 22:44:20 +0000

    Initial completion of configuration updating

--- index.js
+++ index.js
@@ -12,7 +12,6 @@
 var modConfig = require("./modconfig")
 
 // Load required packages
-var yaml = require("yaml")
 var fs = require("fs")
 var prompt = require("prompt")
 var path = require("path")
@@ -44,12 +43,67 @@ complementaryConfig.promptSchema = {
 
 
 complementaryConfig.handleFilePath = function(filePath) {
+	var configFolder = path.join(filePath, "config")
 
+	console.log("Starting config modifications...")
+
+	Object.keys(modConfig.modifications).forEach(function(modificationName) {
+		var modificationData = modConfig.modifications[modificationName]
+
+		complementaryConfig.applyModification(configFolder, modificationData, modificationName)
+	})
+}
+
+complementaryConfig.applyModification = function(configFolder, modificationData, modificationName) {
+	var modificationConfigPath = path.join(configFolder, modificationData["configPath"])
+
+	console.log("Applying " + modificationName + " config changes...")
+
+	if (!fs.existsSync(modificationConfigPath)) {
+		console.log(modificationName + " is not detected, cancelled modification.")
+	} else {
+		try {
+			var unparsedConfig = fs.readFileSync(modificationConfigPath, "utf8")
+			var modifiedConfig = unparsedConfig
+			var modificationCount = 0
+
+			for (var [textToReplace, newText] of Object.entries(modificationData["configModifications"])) {
+				var previousConfig = modifiedConfig
+
+				if (modifiedConfig.contains(textToReplace)) {
+					console.log("contains!")
+				}
+
+				modifiedConfig = modifiedConfig.replace(textToReplace, newText)
+
+				if (modifiedConfig != previousConfig) {
+					modificationCount++
+				}
+
+				fs.writeFile(modificationConfigPath, modifiedConfig, "utf8", function(err) {
+					if (err) {
+						console.log("Failed to run modification " + modificationName + ", error while writing config, cancelled modification.")
+					}
+				})
+			}
+
+			console.log("Successfully ran " + modificationCount + " modifications for modification " + modificationName)
+		} catch (err) {
+			console.log("Failed to run modification " + modificationName + ", error while modifying config, cancelled modification.")
+
+			console.error(err)
+		}
+	}
 }
 
 // Start prompt to get filepath for minecraft from user
 prompt.start()
 
 prompt.get(complementaryConfig.promptSchema, function(err, result) {
+	// User has provided valid filepath, proceed with configuration fixes
+
+	console.log("Valid filepath detected, found required folders.")
+	console.log("Running complementary-config on '" + path.join(result.filePath, "config") + "'")
+
 	complementaryConfig.handleFilePath(result.filePath)
 })
--- modconfig.js
+++ modconfig.js
@@ -1,3 +1,50 @@
 var modConfig = {}
 
+modConfig.modifications = []
+
+modConfig.modifications["Botania 1.12"] = {
+    configPath: "botania.cfg",
+    configModifications: {
+        "B:fancySkybox.enable=true": "B:fancySkybox.enable=false",
+        "B:shaders.enabled=true": "B:shaders.enabled=false"
+    }
+}
+
+modConfig.modifications["Botania 1.16.5"] = {
+    configPath: "botania-client.toml",
+    configModifications: {
+        "shaders = true": "shaders = false",
+        "enabled = true": "enabled = false"
+    }
+}
+
+modConfig.modifications["CC: Tweaked 1.16.5"] = {
+    configPath: "computercraft-client.toml",
+    configModifications: {
+        "monitor_renderer = \"BEST\"": "monitor_renderer=\"VBO\"",
+        "monitor_renderer = \"TBO\"": "monitor_renderer=\"VBO\""
+    }
+}
+
+modConfig.modifications["Astral Sorcery 1.16.5"] = {
+    configPath: "astralsorcery-client.toml",
+    configModifications: {
+        "skyRenderingEnabled = [\"minecraft:overworld\"]": "skyrenderingEnabled = []"
+    }
+}
+
+modConfig.modifications["Advanced Rocketry 1.12.2"] = {
+    configPath: "advRocketry/advancedRocketry.cfg",
+    configModifications: {
+        "B:overworldSkyOverride=true": "B:overworldSkyOverride=false"
+    }
+}
+
+/*modConfig.modifications["Astral Sorcery 1.12"] = {
+    configPath: "astralsorcery.cfg",
+    configModifications: {
+        'S:weakSkyRenders <\n >': 'S:weakSkyRenders < \n0\n>'
+    }
+}*/
+
 module.exports = modConfig
